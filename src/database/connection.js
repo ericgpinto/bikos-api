e7b3064f345114a38bce0d393bfae1555431a363
Éric Pinto <ericgrandopinto@gmail.com>
2021-08-10 15:55:27 -0300

    added connection with mongodb

---
+++ src/database/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://eric:2t3KOxZMGHbRHvQ4@cluster0.cc048.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+mongoose.Promise = global.Promise
+
+module.exports = mongoose
