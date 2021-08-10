65d0d1d63a5b3b21f20238efba0c7ce933902ef3
Éric Pinto <ericgrandopinto@gmail.com>
2021-08-10 15:25:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = require('./src/routes')
+
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+const port = process.env.PORT || '3000'
+
+app.listen(port, () => {
+    console.log('Server running...')
+})
