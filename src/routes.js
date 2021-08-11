722ee09b31420a1932f6906932365ceb071e31b9
Éric Pinto <ericgrandopinto@gmail.com>
2021-08-11 02:28:01 -0300

    authenticating user in application

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const AuthController = require('./controller/AuthController');
 const UserController = require('./controller/UserController');
 const router = express.Router();
 
-router.post('users/register', UserController.register);
+router.post('/users/register', UserController.register);
+router.post('/auth/authenticate', AuthController.authenticate);
 
 module.exports = router
