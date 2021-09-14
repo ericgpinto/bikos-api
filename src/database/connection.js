10ea812b8b550da96a43341f2e24f315f0544964
eric.pinto <eric.pinto@e-unicred.com.br>
2021-09-14 19:46:31 -0300

    refactor: connection with database was changed

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://eric:2t3KOxZMGHbRHvQ4@cluster0.cc048.mongodb.net/user-ms?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://eric:yck4z47ifv7xdqGz@cluster0.cc048.mongodb.net/user-ms?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
